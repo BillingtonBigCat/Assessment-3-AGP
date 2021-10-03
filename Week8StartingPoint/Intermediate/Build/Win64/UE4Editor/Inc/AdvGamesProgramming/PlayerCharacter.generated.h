f22059188d4c8588ca174d5ffb310184c99654ad
BillingtonBigCat <wmajor21@gmail.com>
2021-10-03 14:35:39 +1100

    gjkkj

---
+++ Week8StartingPoint/Intermediate/Build/Win64/UE4Editor/Inc/AdvGamesProgramming/PlayerCharacter.generated.h
@@ -0,0 +1,100 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ADVGAMESPROGRAMMING_PlayerCharacter_generated_h
+#error "PlayerCharacter.generated.h already included, missing '#pragma once' in PlayerCharacter.h"
+#endif
+#define ADVGAMESPROGRAMMING_PlayerCharacter_generated_h
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_SPARSE_DATA
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_RPC_WRAPPERS
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_RPC_WRAPPERS_NO_PURE_DECLS
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_EVENT_PARMS
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_CALLBACK_WRAPPERS
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPlayerCharacter(); \
+	friend struct Z_Construct_UClass_APlayerCharacter_Statics; \
+public: \
+	DECLARE_CLASS(APlayerCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AdvGamesProgramming"), NO_API) \
+	DECLARE_SERIALIZER(APlayerCharacter)
+
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesAPlayerCharacter(); \
+	friend struct Z_Construct_UClass_APlayerCharacter_Statics; \
+public: \
+	DECLARE_CLASS(APlayerCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/AdvGamesProgramming"), NO_API) \
+	DECLARE_SERIALIZER(APlayerCharacter)
+
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APlayerCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APlayerCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APlayerCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APlayerCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APlayerCharacter(APlayerCharacter&&); \
+	NO_API APlayerCharacter(const APlayerCharacter&); \
+public:
+
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APlayerCharacter(APlayerCharacter&&); \
+	NO_API APlayerCharacter(const APlayerCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APlayerCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APlayerCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APlayerCharacter)
+
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_PRIVATE_PROPERTY_OFFSET
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_10_PROLOG \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_EVENT_PARMS
+
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_PRIVATE_PROPERTY_OFFSET \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_SPARSE_DATA \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_RPC_WRAPPERS \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_CALLBACK_WRAPPERS \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_INCLASS \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_PRIVATE_PROPERTY_OFFSET \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_SPARSE_DATA \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_CALLBACK_WRAPPERS \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_INCLASS_NO_PURE_DECLS \
+	Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ADVGAMESPROGRAMMING_API UClass* StaticClass<class APlayerCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Week8StartingPoint_Source_AdvGamesProgramming_PlayerCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Week8StartingPoint/Source/AdvGamesProgramming/HealthComponent.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "AIManager.h"
+#include "EngineUtils.h"
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+	MaxHealth = 100.0f;
+
+	for (TObjectIterator<AAIManager> Itr; Itr; ++Itr)
+	{
+		if (Itr->IsA(AAIManager::StaticClass()))
+		{
+			Manager = *Itr;
+		}
+	}
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentHealth = MaxHealth;
+}
+
+
+// Called every frame
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UHealthComponent::OnTakeDamage(float Damage)
+{
+	CurrentHealth -= Damage;
+		if (CurrentHealth <= 0.0f)
+		{
+			CurrentHealth = 0;
+			OnDeath();
+		}
+}
+
+void UHealthComponent::OnDeath()
+{
+	if (Manager) {
+		Manager->ReduceEnemyEntities();
+	}
+	else
+		UE_LOG(LogTemp, Warning, TEXT("Nope"));
+}
+
+float UHealthComponent::HealthPercentageRemaining()
+{
+	return CurrentHealth / MaxHealth * 100.0f;
+}
+
+
